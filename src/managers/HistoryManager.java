8ccb6ad202a85ed82f44a403e6875fb19270f843
Juri Melnikov <Juri.Melnikov@ivkhk.ee>
2022-10-18 11:26:38 +0300

    Добавили классы инструменты и перенесли в них код из метода run()

---
+++ src/managers/HistoryManager.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managers;
+
+import entity.Book;
+import entity.History;
+import entity.Reader;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Melnikov
+ */
+public class HistoryManager {
+    private final Scanner scanner;
+    private final ReaderManager readerManager;
+    private final BookManager bookManager;
+
+    public HistoryManager() {
+        scanner = new Scanner(System.in);
+        readerManager = new ReaderManager();
+        bookManager = new BookManager();
+    }
+    
+    public History takeOnBook(Reader[] readers, Book[] books){
+        //из списка читателей выбрать номер читателя
+        //из списка книг выбрать номер книги
+        //инициировать поля History
+        //добавить дату выдачи книги
+        System.out.println("Список читателей:");
+        readerManager.printListReaders(readers);
+        System.out.print("Выберите номер читателя из списка:");
+        int numberReader = scanner.nextInt(); scanner.nextLine();
+        
+        System.out.println("Список книг:");
+        bookManager.printListBooks(books);
+        System.out.print("Выберите номер книги из списка:");
+        int numberBook = scanner.nextInt(); scanner.nextLine();
+        History history = new History();
+        history.setBook(books[numberBook - 1]);
+        history.setReader(readers[numberReader - 1]);
+        history.setTakeOnBook(new GregorianCalendar().getTime());
+        return history;
+    }
+}
---
+++ src/managers/ReaderManager.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managers;
+
+import entity.Reader;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Melnikov
+ */
+public class ReaderManager {
+    private final Scanner scanner;
+
+    public ReaderManager() {
+        scanner = new Scanner(System.in);
+    }
+    
+    public Reader createReader(){
+        Reader reader = new Reader();
+        System.out.print("Имя читателя: ");
+        reader.setFirstname(scanner.nextLine());
+        System.out.print("Фамилия читателя: ");
+        reader.setLastname(scanner.nextLine());
+        System.out.print("Телефон читателя: ");
+        reader.setPhone(scanner.nextLine());
+        return reader;
+    } 
+    public void printListReaders(Reader[] readers){
+        for (int i = 0; i < readers.length; i++) {
+            System.out.printf("%d. %s %s. Телефон: %s%n"
+                    ,i+1
+                    ,readers[i].getFirstname()
+                    , readers[i].getLastname()
+                    , readers[i].getPhone()
+            );
+        }
+    }
+}
